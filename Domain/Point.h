8ec660edcc824503d0f8747b0da5a001ece325ce
ludmilanv <ludmilanv@mail.ru>
2024-02-14 16:20:03 +0300

    Add project files.

---
+++ Domain/Point.h
@@ -0,0 +1,19 @@
+#pragma once
+namespace rut::cip::figure 
+{
+	/**
+	* @brief Класс Точка.
+	*/
+	struct Point
+	{
+		/**
+	    * @brief Абсцисса.
+	    */
+		double x;
+
+		/**
+		* @brief Ордината.
+		*/
+		double y;
+	};
+}
